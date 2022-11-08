876dc6f8f1a00b494ae1e159029096ca2b5a1708
Davi vaccarezza <76525758+Stirkexd@users.noreply.github.com>
2022-11-07 21:19:06 -0300

    Adicionando interface
    preciso excluir a interface da classe pessoas

--- Main.java
+++ Main.java
@@ -1,6 +1,38 @@
 class Main {
   public static void main(String[] args) {
+        System.out.println("Por favor digite um numero de 1 a 15");
     
+    System.out.println("----------------------");
+    
+     System.out.println("Fazer login");
+    
+     System.out.println("Inscrever participante");
+    
+     System.out.println("Validar inscricao de participante");
+    
+      System.out.println("Invalidar inscricao de participante");
+    
+      System.out.println("Emitir certificado para participante");
+    
+      System.out.println("Submeter artigo");
+    
+      System.out.println("Enviar avaliacao de artigo");
+    
+      System.out.println("Ver avaliacoes de um artigo");
+    
+      System.out.println("Aceitar artigo");
+    
+      System.out.println("Rejeitar artigo");
+    
+      System.out.println("Listar artigos aceitos em ordem alfabetica");//Pode ser executado por qualquer pessoa
+    
+      System.out.println("Listar artigos negados em ordem alfab´etica");//Pode ser executado por qualquer pessoa
+    
+      System.out.println("Ver dados de um artigo");//Pode ser executado por qualquer pessoa
+    
+     System.out.println("Listar participantes do evento em ordem alfab´etica");
+    
+      System.out.println("sair");
   }
 
 }
